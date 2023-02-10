399311f94a0fa5ffb86de605511466f5da85f35f
mateicaba <mateicaba@gmail.com>
2023-02-10 02:16:21 +0200

    perfect working login and register

--- src/components/RegisterView/index.jsx
+++ src/components/RegisterView/index.jsx
@@ -1,92 +1,125 @@
 import React, { useState } from "react";
-import { Alert, Button, Form, Input, Layout, Typography } from "antd";
+import { Form, Input, Button, Layout, Typography, message } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import axios from "axios";
 import "./style.css";
-const { Content } = Layout;
+import LoginView from "../LoginView";
 
-export default function RegisterView({ onFinish, isLoading, error }) {
-  const [confirmDirty, setConfirmDirty] = useState(false);
+const RegistrationForm = () => {
+  const [form] = Form.useForm();
+  const [loading, setLoading] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
-  const handleConfirmBlur = (e) => {
-    const value = e.target.value;
-    setConfirmDirty(confirmDirty || !!value);
-  };
+  const handleSubmit = async () => {
+    try {
+      setLoading(true);
+      const values = await form.validateFields();
 
-  const compareToFirstPassword = (rule, value, callback) => {
-    if (value && value !== form.getFieldValue("password")) {
-      callback("Two passwords that you enter is inconsistent!");
-    } else {
-      callback();
+      // check if passwords match
+      if (values.password !== values.confirmPassword) {
+        message.error("Passwords do not match");
+        return;
+      }
+
+      // check if username is already taken
+      const { data: users } = await axios.get("http://localhost:3001/users");
+      const takenUsername = users.find(
+        (user) => user.username === values.username
+      );
+      if (takenUsername) {
+        message.error("Username is already taken");
+        return;
+      }
+
+      // save new user
+      const newUser = {
+        id: Date.now(),
+        username: values.username,
+        password: values.password,
+        rented: 0,
+        hosted: 0,
+      };
+      await axios.post("http://localhost:3001/users", newUser);
+      message.success("New user created");
+      form.resetFields();
+    } catch (error) {
+      console.error(error);
+      message.error("An error occurred");
+    } finally {
+      setLoading(false);
     }
   };
 
-  const validateToNextPassword = (rule, value, callback) => {
-    if (value && confirmDirty) {
-      form.validateFields(["confirm"], { force: true });
-    }
-    callback();
+  const checkFormValidity = () => {
+    const values = form.getFieldsValue();
+    return values.username && values.password && values.confirmPassword;
   };
 
   return (
     <Layout>
-      <Content style={{ display: "flex", alignItems: "center" }}>
-        <Form
-          onFinish={onFinish}
-          className="register-form"
-          disabled={isLoading}
-        >
-          <Typography.Title style={{ textAlign: "center" }}>
-            Sign Up
-          </Typography.Title>
-          {error && (
-            <Alert
-              style={{ marginBottom: 10 }}
-              description={error}
-              type="error"
-              closable
-            />
-          )}
-          <Form.Item
-            name="username"
-            requiredMark
-            label="Username"
-            rules={[{ required: true, message: "Username required" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            requiredMark
-            label="Password"
-            rules={[
-              { required: true, message: "Password required" },
-              {
-                validator: validateToNextPassword,
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Form.Item
-            name="confirm"
-            requiredMark
-            label="Confirm Password"
-            dependencies={["password"]}
-            rules={[
-              { required: true, message: "Confirm password required" },
-              {
-                validator: compareToFirstPassword,
-              },
-            ]}
-          >
-            <Input.Password onBlur={handleConfirmBlur} />
-          </Form.Item>
-          <Form.Item style={{ textAlign: "center" }}>
-            <Button htmlType="submit" type="primary">
-              Create Account
-            </Button>
-          </Form.Item>
-        </Form>
-      </Content>
+      <Layout.Content style={{ display: "flex", alignItems: "center" }}>
+        {showLogin ? (
+          <LoginView />
+        ) : (
+          <Form form={form} className="register-form" onFinish={handleSubmit}>
+            <Typography.Title style={{ textAlign: "center" }}>
+              Register
+            </Typography.Title>
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username" },
+              ]}
+            >
+              <Input prefix={<UserOutlined />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item
+              name="confirmPassword"
+              rules={[
+                { required: true, message: "Please confirm your password" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="Confirm Password"
+              />
+            </Form.Item>
+            <Form.Item style={{ width: "100% / 2" }}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!checkFormValidity}
+              >
+                Register
+              </Button>
+            </Form.Item>
+            <Form.Item style={{ width: "100% / 2" }}>
+              <Button
+                htmlType="submit"
+                type="primary"
+                onClick={() => setShowLogin(true)}
+              >
+                Back
+              </Button>
+            </Form.Item>
+          </Form>
+        )}
+      </Layout.Content>
     </Layout>
   );
-}
+};
+
+export default RegistrationForm;
